df4b1821ccec8c34f9bc10183e68c534e83fb02a
fspiekermann <spiekerfa@web.de>
2019-02-06 21:31:52 +0100

    A first structure for vue.js

---
+++ browser/what_my_name/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>Under Construction</h1>
+    <h2>This is the place where the best character tooling for Shadowrun 5 will be created</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
